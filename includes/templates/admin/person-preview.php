e4b7556379205d32464815045a35cd7dbf3c6222
Andrew Fairlie <ferile@Big-Mac-Fairlie.turbospeed.home>
2020-05-30 20:52:32 +0200

    Preview layout updated

--- includes/templates/admin/person-preview.php
+++ includes/templates/admin/person-preview.php
@@ -1,82 +1,79 @@
-<div class="ppl__single-person">
-	<div id="post-ppl-preview" class="ppl__card_wrapper / postbox / js-ppl__loading">
 
-		<header class="ppl__card_header"></header>
+<div id="post-ppl-preview" class="ppl__card_wrapper / postbox / js-ppl__loading">
 
-		<figure class="ppl__card_portrait_wrapper">
-			<img id="ppl__portrait_thumbnail" class="ppl__card_portrait_thumbnail" data-default-src="<?php echo FOOPEOPLE_PORTRAIT_DEFAULT ?>" src="<?php echo FOOPEOPLE_PORTRAIT_DEFAULT ?>0">
-		</figure>
+	<figure class="ppl__card_portrait_wrapper">
+		<img id="ppl__portrait_thumbnail" class="ppl__card_portrait_thumbnail" data-default-src="<?php echo FOOPEOPLE_PORTRAIT_DEFAULT ?>" src="<?php echo FOOPEOPLE_PORTRAIT_DEFAULT ?>0">
+	</figure>
 
-		<div class="ppl__card_details">
+	<div class="ppl__card_details">
 
-			<h1 class="ppl__card_name / ppl__heading" data-pace-people-value="name">
-				<span data-pace-people-value="firstname">Firstname</span>
-				<span data-pace-people-value="surname">Surname</span>
-			</h1>
+		<h1 class="ppl__card_name / ppl__heading" data-pace-people-value="name">
+			<span data-pace-people-value="firstname">Firstname</span>
+			<span data-pace-people-value="surname">Surname</span>
+		</h1>
 
-			<span class="ppl__card_work_title / ppl__heading" data-pace-people-value="name">
-				<span data-pace-people-value="jobtitle">
-					Work title
-				</span>
+		<span class="ppl__card_work_title / ppl__heading" data-pace-people-value="name">
+			<span data-pace-people-value="jobtitle">
+				Work title
 			</span>
+		</span>
 
 
 
-			<div class="ppl__card_particulars">
-				<div class="ppl__card_manager">
-				<i class="ppl_icon-user ppl_icon-spacer "></i>
-					<span data-pace-people-value="manager">
-						Manager
-					</span>
-				</div>
-			</div>
-
-
-			<div class="ppl__card_particulars">
-				<div class="ppl__card_work_role">
-					<i class="ppl_icon-group ppl_icon-spacer"></i>
-					<span data-pace-people-value="team">
-						Team
-					</span>
-				</div>
-			</div>
-
-			<div class="ppl__card_particulars">
-				<span class="ppl__card_location / ppl__item_pipe">
-					<i class="ppl_icon-map-marker ppl_icon-spacer"></i>
-					<span data-pace-people-value="location">
-						LOCATION
-					</span>
+		<div class="ppl__card_particulars">
+			<div class="ppl__card_manager">
+			<i class="ppl_icon-user ppl_icon-spacer "></i>
+				<span data-pace-people-value="manager">
+					Manager
 				</span>
-			</div>
-
-			<div class="ppl__card_particulars">
-				<span class="ppl__card_email / ppl__item_pipe">
-					<a>
-						<i class="ppl_icon-envelope ppl_icon-spacer"></i>
-						<span data-pace-people-value="email">
-							Email
-						</span>
-					</a>
-				</span>
-
-				<span class="ppl__card_contactnumber / ppl__item_pipe">
-					<a>
-						<i class="ppl_icon-phone"></i>
-						<span data-pace-people-value="phonenumber">
-							Contact Number
-						</span>
-					</a>
-				</span>
-			</div>
-
-			<div class="ppl__card_skills_wrapper">
-				<i class="ppl_icon-tag  ppl_icon-spacer"></i>
-				<div class="ppl__card_skills" data-pace-people-value="skills">
-					Skills
-				</div>
 			</div>
 		</div>
 
+
+		<div class="ppl__card_particulars">
+			<div class="ppl__card_work_role">
+				<i class="ppl_icon-group ppl_icon-spacer"></i>
+				<span data-pace-people-value="team">
+					Team
+				</span>
+			</div>
+		</div>
+
+		<div class="ppl__card_particulars">
+			<span class="ppl__card_location / ppl__item_pipe">
+				<i class="ppl_icon-map-marker ppl_icon-spacer"></i>
+				<span data-pace-people-value="location">
+					LOCATION
+				</span>
+			</span>
+		</div>
+
+		<div class="ppl__card_particulars">
+			<span class="ppl__card_email / ppl__item_pipe">
+				<a>
+					<i class="ppl_icon-envelope ppl_icon-spacer"></i>
+					<span data-pace-people-value="email">
+						Email
+					</span>
+				</a>
+			</span>
+
+			<span class="ppl__card_contactnumber / ppl__item_pipe">
+				<a>
+					<i class="ppl_icon-phone"></i>
+					<span data-pace-people-value="phonenumber">
+						Contact Number
+					</span>
+				</a>
+			</span>
+		</div>
+
+		<div class="ppl__card_skills_wrapper">
+			<i class="ppl_icon-tag  ppl_icon-spacer"></i>
+			<div class="ppl__card_skills" data-pace-people-value="skills">
+				Skills
+			</div>
+		</div>
 	</div>
+
 </div>
