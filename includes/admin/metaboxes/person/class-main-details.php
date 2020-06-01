963c360c3b04d0919e08589be7a21ddc3e6c1101
bradvin <bradvin@gmail.com>
2020-06-01 18:46:02 +0100

    added test tab with all field types

--- includes/admin/metaboxes/person/class-main-details.php
+++ includes/admin/metaboxes/person/class-main-details.php
@@ -58,8 +58,18 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\Person\MainDetails' )
 									'row_data' => array(
 										'placeholder' => __( 'Job title', 'foopeople' ),
 									)
+								),
+								array(
+									'id'       => 'manager',
+									'title'    => __( 'Line Manager', 'foopeople' ),
+									'desc'     => __( '', 'foopeople' ),
+									'required' => true,
+									'type'     => 'post_relationship',
+									'default'  => '',
+									'row_data' => array(
+										'placeholder' => __( 'Job title', 'foopeople' ),
+									)
 								)
-
 							)
 						),
 						array(
@@ -67,6 +77,7 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\Person\MainDetails' )
 							'label'  => __( 'Portrait', 'foopeople' ),
 							'icon'   => 'dashicons-format-image',
 							'fields' => array(),
+							'type'   => 'featured_image',
 							'featuredImage' => true
 						),
 						array(
@@ -94,6 +105,7 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\Person\MainDetails' )
 							'id'     => 'teams',
 							'label'  => __( 'Teams', 'foopeople' ),
 							'icon'   => 'dashicons-groups',
+							'type'   => 'taxonomy',
 							'fields' => array(),
 							'taxonomy' => FOOPEOPLE_CT_TEAM
 						),
@@ -101,6 +113,7 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\Person\MainDetails' )
 							'id'     => 'skills',
 							'label'  => __( 'Skills', 'foopeople' ),
 							'icon'   => 'dashicons-tag',
+							'type'   => 'taxonomy',
 							'fields' => array(),
 							'taxonomy' => FOOPEOPLE_CT_SKILL
 						),
@@ -108,9 +121,99 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\Person\MainDetails' )
 							'id'     => 'locations',
 							'label'  => __( 'Locations', 'foopeople' ),
 							'icon'   => 'dashicons-admin-site',
+							'type'   => 'taxonomy',
 							'fields' => array(),
 							'taxonomy' => FOOPEOPLE_CT_LOCATION
-						)
+						),
+						array(
+							'id'     => 'test',
+							'label'  => __( 'Test', 'foopeople' ),
+							'icon'   => 'dashicons-universal-access-alt',
+							'fields' => array(
+								array(
+									'id'       => 'text',
+									'title'    => __( 'Text Field', 'foopeople' ),
+									'desc'     => __( 'A test text field', 'foopeople' ),
+									'type'     => 'text',
+								),
+								array(
+									'id'       => 'number',
+									'title'    => __( 'Number Field', 'foopeople' ),
+									'desc'     => __( 'A test number field', 'foopeople' ),
+									'type'     => 'number',
+								),
+								array(
+									'id'       => 'textarea',
+									'title'    => __( 'Textarea Field', 'foopeople' ),
+									'desc'     => __( 'A test textarea field', 'foopeople' ),
+									'type'     => 'textarea',
+								),
+								array(
+									'id'       => 'checkbox',
+									'title'    => __( 'Checkbox Field', 'foopeople' ),
+									'desc'     => __( 'A test Checkbox field', 'foopeople' ),
+									'type'     => 'checkbox',
+								),
+								array(
+									'id'       => 'select',
+									'title'    => __( 'Select Field', 'foopeople' ),
+									'desc'     => __( 'A test select field', 'foopeople' ),
+									'type'     => 'select',
+									'choices' => array(
+										'option1' => __( 'Option 1', 'foopeople' ),
+										'option2' => __( 'Option 2', 'foopeople' ),
+										'option3' => __( 'Option 3', 'foopeople' ),
+										'option4' => __( 'Option 4', 'foopeople' ),
+									)
+								),
+								array(
+									'id'       => 'radio',
+									'title'    => __( 'Radio Field', 'foopeople' ),
+									'desc'     => __( 'A test radio field', 'foopeople' ),
+									'type'     => 'radio',
+									'choices' => array(
+										'option1' => __( 'Option 1', 'foopeople' ),
+										'option2' => __( 'Option 2', 'foopeople' ),
+										'option3' => __( 'Option 3', 'foopeople' ),
+										'option4' => __( 'Option 4', 'foopeople' ),
+									)
+								),
+								array(
+									'id'       => 'checkboxlist',
+									'title'    => __( 'Checkboxlist Field', 'foopeople' ),
+									'desc'     => __( 'A test checkboxlist field', 'foopeople' ),
+									'type'     => 'checkboxlist',
+									'choices' => array(
+										'option1' => __( 'Option 1', 'foopeople' ),
+										'option2' => __( 'Option 2', 'foopeople' ),
+										'option3' => __( 'Option 3', 'foopeople' ),
+										'option4' => __( 'Option 4', 'foopeople' ),
+									)
+								),
+								array(
+									'id'       => 'colorpicker',
+									'title'    => __( 'Colorpicker Field', 'foopeople' ),
+									'desc'     => __( 'A test colorpicker field', 'foopeople' ),
+									'type'     => 'colorpicker',
+								),
+								array(
+									'id'       => 'htmllist',
+									'title'    => __( 'HTML List Field', 'foopeople' ),
+									'desc'     => __( 'A test html list field', 'foopeople' ),
+									'type'     => 'htmllist',
+									'choices' => array(
+										'option1' => array(
+											'html' => '<img src="https://dummyimage.com/32x32/000/fff&text=1" />',
+											'label' => __( 'Option 1', 'foopeople' ),
+										),
+										'option2' => array(
+											'html' => '<img src="https://dummyimage.com/32x32/000/fff&text=2" />',
+											'label' => __( 'Option 2', 'foopeople' ),
+										)
+									)
+								),
+							)
+						),
 					)
 				) );
 
