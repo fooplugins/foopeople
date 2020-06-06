0278fce0d5623b2cb088958ebb50aa4b34256034
bradvin <bradvin@gmail.com>
2020-06-06 12:11:25 +0100

    updated to cater for more field types

--- includes/admin/metaboxes/class-custom-post-type-metabox.php
+++ includes/admin/metaboxes/class-custom-post-type-metabox.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace FooPlugins\FooPeople\Admin\Metaboxes;
 
 if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox' ) ) {
@@ -9,7 +10,7 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 		private $field_group;
 
 		function __construct( $metabox, $field_group ) {
-			$this->metabox = $metabox;
+			$this->metabox     = $metabox;
 			$this->field_group = $field_group;
 
 			//add the metaboxes for a person
@@ -20,20 +21,80 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 
 			//enqueue assets needed for field groups
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+
+			//handle ajax auto suggest fields
+			add_action( 'wp_ajax_foometafield_suggest', array( $this, 'ajax_handle_autosuggest' ) );
+		}
+
+		/**
+		 * Ajax handler for suggest fields
+		 */
+		function ajax_handle_autosuggest() {
+			if ( wp_verify_nonce( foopeople_sanitize_key( 'nonce' ), 'foometafield_suggest' ) ) {
+				$s     = foopeople_sanitize_text( 'q' );
+				$comma = _x( ',', 'page delimiter' );
+				if ( ',' !== $comma ) {
+					$s = str_replace( $comma, ',', $s );
+				}
+				if ( false !== strpos( $s, ',' ) ) {
+					$s = explode( ',', $s );
+					$s = $s[ count( $s ) - 1 ];
+				}
+				$s = trim( $s );
+
+				$results = array();
+
+				$query_type = foopeople_sanitize_key( 'query_type' );
+				$query_data = foopeople_sanitize_key( 'query_data' );
+
+				if ( 'post' === $query_type ) {
+
+					$posts = get_posts(
+							array(
+									's'              => $s,
+									'posts_per_page' => 5,
+									'post_type'      => $query_data
+							)
+					);
+
+					foreach ( $posts as $post ) {
+						$results[] = $post->post_title;
+					}
+
+				} else if ( 'taxonomy' == $query_type ) {
+
+					$terms = get_terms(
+							array(
+									'search'         => $s,
+									'taxonomy'       => $query_data,
+									'hide_empty'     => false
+							)
+					);
+
+					foreach ( $terms as $term ) {
+						$results[] = $term->name;
+					}
+				}
+
+				echo join( $results, "\n" );
+			}
+
+			wp_die();
 		}
 
 		/**
 		 * Add metaboxe to the CPT
+		 *
 		 * @param $post
 		 */
 		function add_meta_boxes( $post ) {
 			add_meta_box(
-				$this->build_id(),
-				$this->metabox['metabox_title'],
-				array( $this, 'render_metabox' ),
-				$this->metabox['post_type'],
-				isset( $this->metabox['context'] ) ? $this->metabox['context'] : 'normal',
-				isset( $this->metabox['priority'] ) ? $this->metabox['priority'] : 'high'
+					$this->build_id(),
+					$this->metabox['metabox_title'],
+					array( $this, 'render_metabox' ),
+					$this->metabox['post_type'],
+					isset( $this->metabox['context'] ) ? $this->metabox['context'] : 'normal',
+					isset( $this->metabox['priority'] ) ? $this->metabox['priority'] : 'high'
 			);
 		}
 
@@ -47,6 +108,7 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 
 		/**
 		 * Render the metabox contents
+		 *
 		 * @param $post
 		 */
 		public function render_metabox( $post ) {
@@ -57,8 +119,8 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 
 			//render the nonce used to validate when saving the metabox fields
 			?><input type="hidden" name="<?php echo $full_id; ?>_nonce"
-				   id="<?php echo $full_id; ?>_nonce"
-				   value="<?php echo wp_create_nonce( $full_id ); ?>"/><?php
+					 id="<?php echo $full_id; ?>_nonce"
+					 value="<?php echo wp_create_nonce( $full_id ); ?>"/><?php
 
 			//render the tab field group
 			FieldRenderer::render_tabs( $this->field_group, $full_id, $state );
@@ -81,13 +143,13 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 
 			// verify nonce
 			if ( array_key_exists( $full_id . '_nonce', $_POST ) &&
-			     wp_verify_nonce( $_POST[$full_id . '_nonce'], $full_id ) ) {
+				 wp_verify_nonce( $_POST[ $full_id . '_nonce' ], $full_id ) ) {
 
 				//if we get here, we are dealing with the metabox fields
 
 				$action = 'FooPlugins\FooPeople\Admin\Metaboxes\\' . $this->metabox['post_type'] . '\\' . $this->metabox['metabox_id'] . '\\';
 
-				do_action( $action . 'PreSave' , $post_id, $_POST );
+				do_action( $action . 'PreSave', $post_id, $_POST );
 
 				//save the state
 				$state = $this->get_posted_data();
@@ -98,7 +160,7 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 //				$search_index = $person->build_search_index();
 //				update_post_meta( $post_id, PACEPEOPLE_PERSON_META_SEARCH, $search_index );
 
-				do_action( $action . 'PostSave' , $post_id, $_POST );
+				do_action( $action . 'PostSave', $post_id, $_POST );
 			}
 		}
 
@@ -118,7 +180,7 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 			foreach ( $this->field_group['tabs'] as $tab ) {
 				foreach ( $tab['fields'] as $field ) {
 
-					if ( !array_key_exists( $field['id'], $sanitized_data ) ) {
+					if ( ! array_key_exists( $field['id'], $sanitized_data ) ) {
 						//the field had no posted value, check for a default
 						if ( isset( $field['default'] ) ) {
 							$data[ $field['id'] ] = $field['default'];
@@ -149,16 +211,23 @@ if ( ! class_exists( 'FooPlugins\FooPeople\Admin\Metaboxes\CustomPostTypeMetabox
 
 		/***
 		 * Enqueue the assets needed by the settings
+		 *
 		 * @param $hook_suffix
 		 */
-		function enqueue_assets( $hook_suffix ){
-			if( in_array( $hook_suffix, array( 'post.php', 'post-new.php' ) ) ) {
+		function enqueue_assets( $hook_suffix ) {
+			if ( in_array( $hook_suffix, array( 'post.php', 'post-new.php' ) ) ) {
 				$screen = get_current_screen();
 
-				if ( is_object( $screen ) && $this->metabox['post_type'] == $screen->post_type ){
+				if ( is_object( $screen ) && $this->metabox['post_type'] == $screen->post_type ) {
 					// Register, enqueue scripts and styles here
-					wp_enqueue_script( 'foometafields', $this->metabox['plugin_url'] . 'assets/js/foometafields.min.js', array('jquery'), $this->metabox['plugin_version'] );
-					wp_enqueue_style( 'foometafields', $this->metabox['plugin_url'] . 'assets/css/foometafields.min.css', array(), $this->metabox['plugin_version'] );
+					wp_enqueue_script( 'foometafields', $this->metabox['plugin_url'] . 'assets/js/foometafields.min.js', array(
+							'jquery',
+							'suggest',
+							'wp-color-picker'
+					), $this->metabox['plugin_version'] );
+					wp_enqueue_style( 'foometafields', $this->metabox['plugin_url'] . 'assets/css/foometafields.min.css', array(
+							'wp-color-picker'
+					), $this->metabox['plugin_version'] );
 				}
 			}
 		}
